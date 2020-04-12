625ff20077ea16ac4e1fac1cad8d390707bafc91
Matt Mazzola <mattmazzola@live.com>
2020-04-12 15:37:53 -0700

    Add review page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,6 +9,7 @@ import Users from './routes/Users/Users'
 import UsersIndex from './routes/Users/UsersIndex'
 import User from './routes/Users/User/User'
 import Profile from './routes/Profile/Profile'
+import Review from './routes/Review/Review'
 import Test from './routes/Test'
 import PrivateRoute from './components/PrivateRoute'
 import { useAuth0 } from "./react-auth0-spa"
@@ -16,12 +17,15 @@ import history from "./utilities/history"
 import * as client from './services/client'
 import { UserInput } from './models'
 import './App.css'
+import { useSelector, useDispatch } from 'react-redux'
+import * as ProfileSlice from './routes/Profile/profileSlice'
 
-const Navigate = (RRD as any).Navigate
 const Routes = (RRD as any).Routes
 
 function App() {
     const { loading, user, isAuthenticated, getTokenSilently } = useAuth0()
+    const profileState = useSelector(ProfileSlice.selectProfile)
+    const dispatch = useDispatch()
 
     // Attempt to create new user when user is authenticated.
     React.useEffect(() => {
@@ -29,6 +33,7 @@ function App() {
             try {
                 return await client.getUser(userInput.id)
             }
+            // Intentionally catch 404 rejection. This is signal that use does not exist yet.
             catch (e) {
                 const error: Error = e
                 console.error(error)
@@ -38,14 +43,26 @@ function App() {
             }
         }
 
+        async function getTokenData() {
+            const t = await getTokenSilently()
+            const tokenData = await client.verify(t)
+            console.log(`ID Token: `, user)
+            console.log(`Access Token: `, tokenData)
+
+            dispatch(ProfileSlice.setAccessTokenData({ tokenData }))
+        }
+
         if (user && isAuthenticated) {
             createUserIfNotExist({
                 id: user.sub,
                 name: user.name,
             })
+
+            if (profileState.accessTokenData === undefined) {
+                getTokenData()
+            }
         }
-        // eslint-disable-next-line
-    }, [user, isAuthenticated])
+    }, [isAuthenticated, getTokenSilently])
 
     if (loading) {
         return <div>Loading...</div>
@@ -76,12 +93,20 @@ function App() {
                             <RRD.Route path="/" element={<UsersIndex />} />
                             <RRD.Route path=":userId" element={<User />} />
                         </RRD.Route>
-                        <RRD.Route path="debug" element={<Debug />} />
                         <PrivateRoute path="profile" element={<Profile />} />
+                        <RRD.Route path="debug" element={<Debug />} />
+                        <RRD.Route path="review" element={<Review />} />
                     </Routes>
                 </main>
                 <footer>
-                    <RRD.NavLink to="/debug">Debug</RRD.NavLink>
+                    SC2IQ © 2020
+                    {profileState.accessTokenData?.permissions.includes('write:all')
+                        && (
+                            <div>
+                                <RRD.NavLink to="/debug">Debug</RRD.NavLink> &nbsp;
+                                <RRD.NavLink to="/review">Review</RRD.NavLink>
+                            </div>
+                        )}
                 </footer>
             </div>
         </RRD.Router>
---
+++ client/src/routes/Review/Review.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import * as Auth0 from "../../react-auth0-spa"
+import * as client from '../../services/client'
+import * as models from '../../models'
+
+type Props = {
+}
+
+const Review: React.FC<Props> = (props) => {
+    return (
+        <div>
+            <h1>Review</h1>
+        </div>
+    )
+}
+
+export default Review
