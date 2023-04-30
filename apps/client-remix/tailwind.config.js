fa97c2a8a732be34f708b2647e78eb2294cfa477
Matt Mazzola <mattmazzola@live.com>
2023-04-30 13:52:30 -0700

    Add Inter font theme

--- apps/client-remix/tailwind.config.js
+++ apps/client-remix/tailwind.config.js
@@ -1,8 +1,15 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: ["./app/**/*.{ts,tsx,jsx,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
