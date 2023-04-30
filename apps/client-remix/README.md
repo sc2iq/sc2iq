9a60ce39964280b867ca1663dacb6827565e88d8
Matt Mazzola <mattmazzola@live.com>
2023-04-30 14:29:16 -0700

    Add Prisma

--- apps/client-remix/README.md
+++ apps/client-remix/README.md
@@ -1,2 +1,38 @@
 # SC2IQ
 
+## Setup Local Prisma Database Server
+
+- https://remix.run/docs/en/1.15.0/tutorials/jokes#set-up-prisma
+- https://www.prisma.io/docs/concepts/database-connectors/sql-server
+
+
+[Prisma Docker Docs](https://www.prisma.io/docs/concepts/database-connectors/sql-server/sql-server-docker)
+
+```
+docker run -e 'ACCEPT_EULA=Y' -e 'MSSQL_SA_PASSWORD=<YourStrong@Passw0rd>' -p 1433:1433 --name sql1 -d mcr.microsoft.com/mssql/server:2019-latest
+```
+
+```
+docker exec -it sql1 "bash"
+```
+
+```
+/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P "<YourStrong@Passw0rd>"
+```
+
+```
+CREATE DATABASE schultztables
+GO
+```
+
+```
+npx prism db push
+```
+
+## Use Prisma to execute command against DB
+
+<https://www.prisma.io/docs/reference/api-reference/command-reference#db-execute>
+
+```
+prisma db execute --file ./prisma/scripts/create-dbs.sql
+```
--- apps/client-remix/app/routes/_index.tsx
+++ apps/client-remix/app/routes/_index.tsx
@@ -1,7 +1,9 @@
 import { BeakerIcon } from '@heroicons/react/24/solid'
-import { LoaderArgs, V2_MetaFunction, json } from "@remix-run/node"
+import { ActionArgs, LoaderArgs, V2_MetaFunction, json, redirect } from "@remix-run/node"
 import { Form, Link, useLoaderData } from "@remix-run/react"
+import z from "zod"
 import { auth, getSession } from "~/services/auth.server"
+import { db } from "~/services/db.server"
 
 type LoaderError = { message: string } | null
 
@@ -9,10 +11,12 @@ export const loader = async ({ request }: LoaderArgs) => {
   const profile = await auth.isAuthenticated(request)
   const session = await getSession(request.headers.get("Cookie"))
   const error = session.get(auth.sessionErrorKey) as LoaderError
+  const questions = await db.question.findMany()
 
   return json({
-      profile,
-      error,
+    profile,
+    error,
+    questions,
   })
 }
 
@@ -21,8 +25,45 @@ export const meta: V2_MetaFunction = ({ matches }) => {
   return [{ title: `${rootTitle} - Home` }]
 }
 
+const formNames = {
+  create: 'formNameQuestionCreate',
+}
+const formQuestionCreateSchema = z.object({
+  question: z.string(),
+  answer1: z.string(),
+  answer2: z.string(),
+  answer4: z.string(),
+  answer3: z.string(),
+})
+
+export const action = async ({ request }: ActionArgs) => {
+  const rawForm = await request.formData()
+  const formData = Object.fromEntries(rawForm)
+  const formName = formData.formName as string
+
+  if (formNames.create == formName) {
+    const questionInput = formQuestionCreateSchema.parse({
+      question: formData.question as string,
+      answer1: formData.answer1 as string,
+      answer2: formData.answer2 as string,
+      answer3: formData.answer3 as string,
+      answer4: formData.answer4 as string,
+    })
+    const question = await db.question.create({
+      data: {
+        ...questionInput,
+        answerIndex: 0,
+      }
+    })
+
+    return redirect(`/?questionId=${question.id}`)
+  }
+
+  return null
+}
+
 export default function Index() {
-  const { profile, error } = useLoaderData<typeof loader>()
+  const { profile, error, questions } = useLoaderData<typeof loader>()
   const hasProfile = profile !== null && typeof profile === 'object'
 
   return (
@@ -41,6 +82,40 @@ export default function Index() {
           </div>
           <div>You must sign in before you play the game!</div>
         </>}
+      <Form method="post">
+        <div>
+          <label htmlFor='question'>Question: </label>
+          <input type="text" placeholder='How much health does a Marine have?' id="question" name="question" required />
+        </div>
+        <div>
+          <label htmlFor='answer1'>Answer 1: </label>
+          <input type="text" id="answer1" name="answer1" required />
+        </div>
+        <div>
+          <label htmlFor='answer2'>Answer 2: </label>
+          <input type="text" id="answer2" name="answer2" required />
+        </div>
+        <div>
+          <label htmlFor='answer3'>Answer 3: </label>
+          <input type="text" id="answer3" name="answer3" required />
+        </div>
+        <div>
+          <label htmlFor='answer4'>Answer 4: </label>
+          <input type="text" id="answer4" name="answer4" required />
+        </div>
+        <div>
+          <input type="hidden" name="formName" value={formNames.create} />
+          <button type="submit">Create</button>
+        </div>
+
+      </Form>
+      <div>
+        {questions.map(question => {
+          return <div key={question.id}>
+            <h4>{question.question}</h4>
+          </div>
+        })}
+      </div>
     </div>
   )
 }
