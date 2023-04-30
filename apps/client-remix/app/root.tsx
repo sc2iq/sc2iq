a7382762bb67ac2a5a2eaee73b2118bc8014474a
Matt Mazzola <mattmazzola@live.com>
2023-04-30 13:02:51 -0700

    Add basic tailwincss

--- apps/client-remix/app/root.tsx
+++ apps/client-remix/app/root.tsx
@@ -1,3 +1,4 @@
+import type { LinksFunction } from "@remix-run/node" // or cloudflare/deno
 import {
   Links,
   LiveReload,
@@ -5,23 +6,72 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
-} from "@remix-run/react";
+  V2_MetaFunction,
+  useRouteError,
+} from "@remix-run/react"
+import { V2_ErrorBoundaryComponent } from "@remix-run/react/dist/routeModules"
+import styles from "./styles/tailwind.css"
+
+export const links: LinksFunction = () => [
+  { rel: "stylesheet", href: styles },
+]
+
+export const meta: V2_MetaFunction = () => {
+  return [{ title: "SC2IQ" }]
+}
+
+export const ErrorBoundary: V2_ErrorBoundaryComponent = () => {
+  const error = useRouteError()
+  console.error(error)
+  return (
+    <AppComponent>
+      <h1 className="text-xl font-bold">Error!</h1>
+      {error instanceof Error
+        ? <p>{error.message}</p>
+        : <p>{JSON.stringify(error)}</p>
+      }
+    </AppComponent>
+  )
+}
 
 export default function App() {
   return (
-    <html lang="en">
+    <AppComponent>
+      <Outlet />
+    </AppComponent>
+  )
+}
+
+const AppComponent: React.FC<React.PropsWithChildren> = ({ children }) => {
+  return (
+    <html lang="en" className="min-h-full">
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <Meta />
         <Links />
       </head>
-      <body>
-        <Outlet />
+      <body className="h-screen flex flex-col bg-slate-50 font-sans subpixel-antialiased">
+        <header className="bg-slate-600 border-b border-b-slate-700 text-white">
+          <div className="container mx-auto flex gap-x-4 justify-between items-center py-8">
+            <h1 className="text-3xl ">SC2IQ</h1>
+          </div>
+        </header>
+        <main className="container mx-auto flex-1 py-6">
+          {children}
+        </main>
+        <footer className="bg-slate-600 border-b border-b-slate-700 text-white">
+          <div className="container mx-auto py-12">
+            <h2 className="text-lg">References</h2>
+            <ul className="list-disc ml-6">
+              <li><a href="https://learn.microsoft.com/en-us/azure/machine-learning/?view=azureml-api-2" target="_blank">Azure Machine Learning</a></li>
+            </ul>
+          </div>
+        </footer>
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
       </body>
     </html>
-  );
+  )
 }
