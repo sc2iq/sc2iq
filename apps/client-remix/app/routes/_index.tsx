9b695e7f793c997e6234ebc199ae49fe931bfc05
Matt Mazzola <mattmazzola@live.com>
2023-06-11 10:49:13 -0700

    Change home page to dashboard

--- apps/client-remix/app/routes/_index.tsx
+++ apps/client-remix/app/routes/_index.tsx
@@ -45,36 +45,66 @@ export default function IndexRoute() {
 
   return (
     <>
-      <h1 className="text-center font-semibold text-3xl py-2">Welcome to SC2IQ!</h1>
-      {!hasProfile
-        && (<>
-          {loaderData.error ? <div>{loaderData.error.message}</div> : null}
+      {loaderData.error
+        && (
+          <>
+            <h1>Error:</h1>
+            <div>{loaderData.error.message}</div>
+          </>)}
+      {hasProfile
+        ? (<>
+          <h1 className="text-center font-semibold text-3xl py-2">Welcome back, {loaderData.profile?.displayName}!</h1>
+          <h1 className="font-semibold text-3xl py-2">Personal Stats</h1>
+          <div className="flex flex-col gap-2 font-semibold text-2xl">
+            <div className="flex gap-4">
+              <div>SC2IQ:</div>
+              <div>3492</div>
+            </div>
+            <div className="flex gap-4">
+              <div>Votes:</div>
+              <div>3</div>
+            </div>
+          </div>
+        </>)
+        : (<>
+          <h1 className="text-center font-semibold text-3xl py-2">Welcome to SC2IQ!</h1>
           <div className="flex justify-center">
             <Form method="post" action="/auth">
               <button type="submit" className="px-5 py-4 border border-slate-500 bg-slate-400 rounded-md text-4xl font-semibold my-4">Log In</button>
             </Form>
           </div>
+          <div className="grid grid-flow-col gap-6">
+            <div>
+              <h2 className="font-semibold text-3xl py-2">What is SC2IQ?</h2>
+              <p className="leading-7">
+                SC2IQ attempts to improve the way feedback is collected from the community.<br />
+                We measure knowledge of the game to weigh the opinions.<br />
+                The premise is that the more knowlege you have about a subject the more value your opinion should hold.<br />
+                This metrics allows amplifying the signal of knowledgable players from the noise less knowledage players
+              </p>
+            </div>
+            <div>
+              <h2 className="font-semibold text-3xl py-2">How do I use SC2IQ?</h2>
+              <p className="leading-7">
+                1. Study <Link to="questions" className="underline underline-offset-2">questions</Link> to learn about Starcraft<br />
+                2. Take <Link to="test" className="underline underline-offset-2">tests</Link> to establish your StarCraft 2 Intelligence metric and collect Votes!<br />
+                3. Vote on <Link to="polls" className="underline underline-offset-2">polls</Link> influence discussion and perhaps balance changes!
+              </p>
+            </div>
+          </div>
         </>)}
 
-      <div className="grid grid-flow-col gap-6">
-        <div>
-          <h2 className="font-semibold text-3xl py-2">What is SC2IQ?</h2>
-          <p className="leading-7">
-            SC2IQ attempts to improve the way feedback is collected from the community.<br />
-            We measure knowledge of the game to weigh the opinions.<br />
-            The premise is that the more knowlege you have about a subject the more value your opinion should hold.<br />
-            This metrics allows amplifying the signal of knowledgable players from the noise less knowledage players
-          </p>
+      <>
+
+        <h1 className="font-semibold text-3xl py-2">Recently Added Questions:</h1>
+        <div className="flex flex-col gap-2 text-2xl">
+          Lorem Ipsum
         </div>
-        <div>
-          <h2 className="font-semibold text-3xl py-2">How do I use SC2IQ?</h2>
-          <p className="leading-7">
-            1. Study <Link to="questions" className="underline underline-offset-2">questions</Link> to learn about Starcraft<br />
-            2. Take <Link to="test" className="underline underline-offset-2">tests</Link> to establish your StarCraft 2 Intelligence metric and collect Votes!<br />
-            3. Vote on <Link to="polls" className="underline underline-offset-2">polls</Link> influence discussion and perhaps balance changes!
-          </p>
+        <h1 className="font-semibold text-3xl py-2">Recently Added Polls:</h1>
+        <div className="flex flex-col gap-2 text-2xl">
+          Lorem Ipsum
         </div>
-      </div>
+      </>
     </>
   )
 }
