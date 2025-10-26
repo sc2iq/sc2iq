41414f0e52f9c75737f853f6a278274affea2416
Matthew Mazzola <mattmazzola@live.com>
2025-10-26 03:58:56 +0000

    update scripts and readme

--- README.md
+++ README.md
@@ -4,10 +4,16 @@
 
 ### Preparation
 
-```powershell
+```bash
 az login
+az account set -n "Matt Mazzola - Personal Projects Recovered"
+az account show --query "name" -o tsv
 az acr login --name sharedklgoyiacr
+```
 
+### What If Deployment
+
+```pwsh
 .\scripts\deploy.ps1
 ```
 
--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -1,17 +1,36 @@
 Param([switch]$WhatIf = $True)
 
-echo "PScriptRoot: $PScriptRoot"
-$repoRoot = If ('' -eq $PScriptRoot) {
-  "$PSScriptRoot/../.."
+
+$scriptPath = $MyInvocation.MyCommand.Path
+$scriptDir = Split-Path $scriptPath
+
+# Find repo root by searching upward for README.md
+$currentDir = $scriptDir
+$repoRoot = $null
+while ($currentDir -and -not $repoRoot) {
+  if (Test-Path (Join-Path $currentDir "README.md")) {
+    $repoRoot = $currentDir
+  }
+  else {
+    $currentDir = Split-Path $currentDir
+  }
 }
-Else {
-  "."
+if (-not $repoRoot) {
+  throw "Could not find repo root (no README.md found in parent directories)."
 }
 
+echo "Script Path: $scriptPath"
+echo "Script Dir: $scriptDir"
 echo "Repo Root: $repoRoot"
 
-Import-Module "C:/repos/shared-resources/pipelines/scripts/common.psm1" -Force
-Import-Module "$repoRoot/scripts/common.psm1" -Force
+$sharedModulePath = Resolve-Path "$repoRoot/../../shared-resources/pipelines/scripts/common.psm1"
+$localModulePath = Resolve-Path "$repoRoot/scripts/common.psm1"
+
+echo "Shared Module Path: $sharedModulePath"
+echo "Local Module Path: $localModulePath"
+
+Import-Module $sharedModulePath -Force
+Import-Module $localModulePath -Force
 
 $inputs = @{
   "WhatIf" = $WhatIf
